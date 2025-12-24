34493b6cbc4e2c8f245c98e1b00358f0a9233475
fandynasution <ahmad.ariffandy@ifca.co.id>
2025-12-24 15:36:17 +0700

    123

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
@@ -7,5 +8,6 @@ export class AppController {
 
   @Get()
   @Redirect('/api-documentation', 302)
+  @ApiExcludeEndpoint()
   redirectToSwagger() {}
 }
