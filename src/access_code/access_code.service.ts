45b52ed2d349dc77a6adb14ecd33aeda6ac5bed6
fandynasution <ahmad.ariffandy@ifca.co.id>
2025-12-29 11:10:30 +0700

    Access Code

--- src/access_code/access_code.service.ts
+++ src/access_code/access_code.service.ts
@@ -1,19 +1,31 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
-import { PostgresService } from "../database/postgres.service";
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PostgresService } from '../database/postgres.service';
 
 @Injectable()
 export class AccessCodeService {
   constructor(
-    private readonly PostgresService: PostgresService,
+    private readonly postgresService: PostgresService,
   ) {}
 
   async findByCode(code: string) {
     const query = `
-    SELECT * FROM company_access WHERE code = $1`;
-    const result = await this.PostgresService.query(query);
+      SELECT
+        access_code,
+        channel,
+        sender
+      FROM company_access
+      WHERE access_code = $1
+      LIMIT 1
+    `;
+    console.log(query);
+    const result = await this.postgresService.query(
+      query,
+      [code], // ✅ kirim parameter
+    );
     if (result.length === 0) {
       throw new NotFoundException('Access code not found');
     }
+
     return result[0];
   }
 }
