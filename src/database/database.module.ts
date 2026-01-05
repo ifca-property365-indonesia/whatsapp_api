2e60d2353eff9e0bd2d7e99374121d38b56a3916
fandynasution <ahmad.ariffandy@ifca.co.id>
2026-01-05 11:47:52 +0700

    9512368420

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PostgresService } from './postgres.service';
+import { MssqlService } from './mssqlserver.service';
 
 @Module({
-  providers: [PostgresService],
-  exports: [PostgresService],
+  providers: [PostgresService, MssqlService],
+  exports: [PostgresService, MssqlService],
 })
 export class DatabaseModule {}
